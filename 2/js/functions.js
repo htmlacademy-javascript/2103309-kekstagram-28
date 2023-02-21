c31500fd7a2f624e17b558c00d09d4a912bf7216
Keks <keks@htmlacademy.ru>
2023-02-21 05:43:04 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -1,50 +1,44 @@
 // Проходит ли строка по длинне?
-function findLenght(str) {
-  letters = 20;
-  if (str.length >= letters) {
-    return true;
-    }
-      return false;
+function findLenght(str, letters) {
+  return str.length >= letters;
 }
-findLenght('Теплый пушистый котенок спит, свернулся в клубочек и мурчит.', letters)
+findLenght('Теплый пушистый котенок спит, свернулся в клубочек и мурчит.', 5);
 
 // Полидром ли этот текст (и текст с пробелами)?
 function isItPalidrom(str) {
   str = str.toLowerCase().replaceAll(/[^а-яa-z1-9]/g, '');
 
-  let index = str.length - 1;
+  const index = str.length - 1;
   for (let i = 0; i < str.length / 2; i++) {
-      if (str[i] !== str[index - i]) {
-          return false;
-      }
+    if (str[i] !== str[index - i]) {
+      return false;
+    }
   }
   return true;
 }
-isItPalidrom('level')
+isItPalidrom('level');
 
 // Есть ли в этой строке цифры?
 function isItNumber(str) {
-	let string = parseInt(str.replaceAll(/[^\d]/g, ''))
+  const string = parseInt(str.replaceAll(/[^\d]/g, ''), 10);
 
-  if (typeof(string) === true) {
-    console.log(Number(string))
-  } console.log(string)
+  if (typeof string !== 'string') {
+    return string;
+  }
 }
-isItNumber('1 мяу')
-
+isItNumber('1 мяу');
 
 // Плюс префикс
 function originalStr(strOrig, strLenght, strAdd) {
-  prefixLength = strLenght - strOrig.length
-    if (prefixLength <= 0) {
-    return strOrig
+  const prefixLength = strLenght - strOrig.length;
+  if (prefixLength <= 0) {
+    return strOrig;
   }
 
-  let prefix = ""
-    for (let i = 0; i < prefixLength; i++) {
-    prefix += strAdd[i % strAdd.length]
+  let prefix = '';
+  for (let i = 0; i < prefixLength; i++) {
+    prefix += strAdd[i % strAdd.length];
   }
-  return prefix + strOrig
-  }
-
-originalStr('q', 4, 'werty')
+  return prefix + strOrig;
+}
+originalStr('q', 4, 'werty');
