099082525774b835d84098f2ada7e4fef7b0f82a
Antonina <68737448+MrsGermy@users.noreply.github.com>
2023-02-27 14:23:50 +0700

    Поправляю недочеты

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 //Магические числа
-const minIdNumber = 1;
-const maxIdNumber = 25;
-const maxAvatarNumber = 6;
-const minLikeNumber = 15;
-const maxLikeNumber = 200;
-const maxCommentNumber = 1000;
-const maxArrayLenght = 25;
+const MIN_ID_NUMBER = 1;
+const MAX_ID_NUMBER = 25;
+const MAX_AVATAR_NUMBER = 6;
+const MIN_LIKE_NUMBER = 15;
+const MAX_LIKE_NUMBER = 200;
+const MAX_COMMENT_NUMBER = 1000;
+const MAX_ARRAY_LENGHT = 25;
 
 // строка — описание фотографии.
 const DESCRIPTIONS = [
@@ -22,7 +22,7 @@ const DESCRIPTIONS = [
 ];
 
 // текст комментария, оставленного пользователями.
-const MASSAGES = [
+const MESSAGES = [
   'Всё отлично!',
   'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
@@ -72,28 +72,28 @@ const getId = (min, max) => {
   };
 };
 
-const generatePhotoId = getId(minIdNumber, maxIdNumber);
-const generateId = getId(minIdNumber, maxIdNumber);
+const generatePhotoId = getId(MIN_ID_NUMBER, MAX_ID_NUMBER);
+const generateId = getId(MIN_ID_NUMBER, MAX_ID_NUMBER);
 
 // «разделяй и властвуй» =)
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 // массив объектов с комментариями.
-const userComments = () => ({
-  id: getRandomInteger(minIdNumber, maxCommentNumber),
-  avatar: `img/avatar-${ getRandomInteger(minIdNumber, maxAvatarNumber) }.svg`,
-  message: getRandomArrayElement(MASSAGES),
+const getUserComments = () => ({
+  id: getRandomInteger(MIN_ID_NUMBER, MAX_COMMENT_NUMBER),
+  avatar: `img/avatar-${ getRandomInteger(MIN_ID_NUMBER, MAX_AVATAR_NUMBER) }.svg`,
+  message: getRandomArrayElement(MESSAGES),
   name: getRandomArrayElement(NAMES)
 });
 
-// массив объектов с фотографиями пользоваиелей.
-const userPhotos = () => ({
+// массив объектов с фотографиями пользователей.
+const getUserPhotos = () => ({
   id: generateId(),
   url: `photos/${ generatePhotoId() }.jpg`,
   description: getRandomArrayElement(DESCRIPTIONS),
-  likes: getRandomInteger(minLikeNumber, maxLikeNumber),
-  comments: userComments()
+  likes: getRandomInteger(MIN_LIKE_NUMBER, MAX_LIKE_NUMBER),
+  comments: getUserComments()
 });
 
 //создаем массив из 25 объектов-фотографий.
-Array.from({length: maxArrayLenght}, userPhotos);
+Array.from({length: MAX_ARRAY_LENGHT}, getUserPhotos);
