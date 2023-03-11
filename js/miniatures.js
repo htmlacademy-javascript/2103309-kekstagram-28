4ee7c2799c8acb76f7a14f332ba3d07ab0bede6b
Antonina <68737448+MrsGermy@users.noreply.github.com>
2023-03-11 09:16:36 +0700

    Правка Lint

--- js/miniatures.js
+++ js/miniatures.js
@@ -11,13 +11,13 @@ const fragment = document.createDocumentFragment();
 
 const renderPhotos = () => {
 
-newPhotos.forEach(({url, likes, comments}) => {
-  const newElement = templateMiniatures.cloneNode(true);
-  newElement.querySelector('img').src = url;
-  newElement.querySelector('.picture__comments').textContent = comments.message.length;
-  newElement.querySelector('.picture__likes').textContent = likes;
+  newPhotos.forEach(({url, likes, comments}) => {
+    const newElement = templateMiniatures.cloneNode(true);
+    newElement.querySelector('img').src = url;
+    newElement.querySelector('.picture__comments').textContent = comments.message.length;
+    newElement.querySelector('.picture__likes').textContent = likes;
 
-  fragment.appendChild(newElement);
+    fragment.appendChild(newElement);
   });
 
   newContainer.appendChild(fragment);
